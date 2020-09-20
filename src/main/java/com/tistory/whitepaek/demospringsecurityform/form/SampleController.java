17c3510ffd64c6641f5fd6ec4cbfdb7cbcc6f3bf
WHITEPAEK <atomsjp@gmail.com>
2020-09-20 13:58:26 +0900

    정적 리소스 권한 설정 및 비동기 쓰레드 SecurityContext 공유 설정
    + 정적 리소스 권한 설정 (SecurityConfig - Line 28)
    + 비동기 쓰레드 SecurityContext 공유 (SecurityConfig - Line 42)

--- src/main/java/com/tistory/whitepaek/demospringsecurityform/form/SampleController.java
+++ src/main/java/com/tistory/whitepaek/demospringsecurityform/form/SampleController.java
@@ -1,13 +1,26 @@
 package com.tistory.whitepaek.demospringsecurityform.form;
 
+import com.tistory.whitepaek.demospringsecurityform.account.AccountContext;
+import com.tistory.whitepaek.demospringsecurityform.account.AccountRepository;
+import com.tistory.whitepaek.demospringsecurityform.common.SecurityLogger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.concurrent.Callable;
 
 @Controller
 public class SampleController {
+
+    private final SampleService sampleService;
+    private final AccountRepository accountRepository;
+
+    public SampleController(SampleService sampleService, AccountRepository accountRepository) {
+        this.sampleService = sampleService;
+        this.accountRepository = accountRepository;
+    }
 
     @GetMapping("/")
     public String index(Model model, Principal principal) {
@@ -29,6 +42,8 @@ public class SampleController {
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
         model.addAttribute("message", "Hello " + principal.getName());
+        AccountContext.setAccount(accountRepository.findByUsername(principal.getName()));
+        sampleService.dashboard();
         return "dashboard";
     }
 
@@ -36,6 +51,31 @@ public class SampleController {
     public String admin(Model model, Principal principal) {
         model.addAttribute("message", "Hello Admin, " + principal.getName());
         return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model, Principal principal) {
+        model.addAttribute("message", "Hello User, " + principal.getName());
+        return "user";
+    }
+
+    @GetMapping("/async-handler")
+    @ResponseBody
+    public Callable<String> asyncHandler() {
+        SecurityLogger.log("MVC");
+        return () -> {
+            SecurityLogger.log("Callable");
+            return "Async Handler";
+        };
+    }
+
+    @GetMapping("/async-service")
+    @ResponseBody
+    public String asyncService() {
+        SecurityLogger.log("MVC, before async service");
+        sampleService.asyncService();
+        SecurityLogger.log("MVC, after async service");
+        return "Async Service";
     }
 
 }
---
+++ src/main/java/com/tistory/whitepaek/demospringsecurityform/form/SampleService.java
@@ -0,0 +1,26 @@
+package com.tistory.whitepaek.demospringsecurityform.form;
+
+import com.tistory.whitepaek.demospringsecurityform.common.SecurityLogger;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SampleService {
+
+    public void dashboard() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        System.out.println(authentication);
+        System.out.println(userDetails.getUsername());
+    }
+
+    @Async
+    public void asyncService() {
+        SecurityLogger.log("Async Service");
+        System.out.println("Async service is called.");
+    }
+}
